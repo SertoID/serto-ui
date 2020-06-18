f5e49ce8e3126f33a944fde139eea7b70a42789c
Toby Fox <tobyfox@gmail.com>
2020-06-17 19:21:45 -0700

    fix: VC display updates to handle News Article

--- src/components/Tenant/Credentials.tsx
+++ src/components/Tenant/Credentials.tsx
@@ -23,17 +23,26 @@ export const Credentials: React.FunctionComponent = (props) => {
             const issuanceDate = dateTimeFormat(new Date(jwtDecoded.iat * 1000));
             const issuer = ellipsis("0x" + jwtDecoded.sub.split("0x").pop(), 5, 3);
 
+            // @TODO Temporarily hard-coding some of this stuff, we need a cleverer way to get and show credential info obviously, probably linked to schema detection
+            const attributes =
+              jwtDecoded.vc.credentialSubject.publishedContent ||
+              jwtDecoded.vc.credentialSubject.foo ||
+              jwtDecoded.vc.credentialSubject;
+            const title = jwtDecoded.vc.credentialSubject.publishedContent
+              ? "News Article"
+              : jwtDecoded.vc.credentialSubject.title || "Know Your Customer Check"; // hardcoding for demo purposes, add title in credentialSubject for demo
+
             console.log(jwtDecoded);
 
             return (
               <Credential
                 key={i}
-                attributes={jwtDecoded.vc.credentialSubject.foo}
+                attributes={attributes}
                 credentialType={jwtDecoded.vc.type[0]}
                 issuanceDate={issuanceDate}
                 issuer={issuer}
                 jwt={jwt}
-                title={jwtDecoded.vc.credentialSubject.title || "Know Your Customer Check"} // hardcoding for demo purposes, add title in credentialSubject for demo
+                title={title}
                 viewType={CredentialViewTypes.COLLAPSIBLE}
               />
             );
