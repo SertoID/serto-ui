a193d741837f4e381f2d8b53b99c18e964768b63
Toby Fox <tobyfox@gmail.com>
2020-07-20 21:17:56 -0700

    chore: Use SWR for loading identifiers

--- src/components/Tenant/TenantPage.tsx
+++ src/components/Tenant/TenantPage.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
+import useSWR, { mutate } from "swr";
 import { routes } from "../../constants";
-import { Box, Button, Modal, Card, Tooltip } from "rimble-ui";
+import { Box, Button, Modal, Card, Tooltip, Loader } from "rimble-ui";
 import { TrustAgencyContext } from "../../context/TrustAgentProvider";
 import { TrustAgencyService } from "../../services/TrustAgencyService";
 import { IssueVc } from "./IssueVc";
@@ -10,18 +11,17 @@ export const TenantPage: React.FunctionComponent = (props) => {
   const TrustAgent = React.useContext<TrustAgencyService>(TrustAgencyContext);
 
   const [error, setError] = React.useState<string | undefined>();
-  const [identifiers, setIdentifiers] = React.useState<any[]>([]);
   const [isIssueModalOpen, setIsIssueModalOpen] = React.useState(false);
 
-  const getIdentifiers = React.useCallback(async () => {
-    try {
-      const data = await TrustAgent.getTenantIdentifiers();
-      setIdentifiers(data);
-    } catch (err) {
-      console.error("failed to get identifiers:", err);
-      setError(err.message);
-    }
-  }, [TrustAgent]);
+  const {
+    data: identifiers,
+    error: getIdentifiersError,
+    isValidating: identifiersLoading,
+  } = useSWR("/v1/tenant/identifiers", () => TrustAgent.getTenantIdentifiers());
+  if (getIdentifiersError) {
+    console.error("failed to get identifiers:", getIdentifiersError);
+  }
+  const hasIdentifier = !!(identifiers && identifiers[0]);
 
   async function createIdentifier() {
     try {
@@ -31,12 +31,8 @@ export const TenantPage: React.FunctionComponent = (props) => {
       setError(err.message);
       return;
     }
-    getIdentifiers();
+    mutate("/v1/tenant/identifiers");
   }
-
-  React.useEffect(() => {
-    getIdentifiers();
-  }, [getIdentifiers]);
 
   return (
     <GlobalLayout url={routes.TENANT}>
@@ -46,20 +42,19 @@ export const TenantPage: React.FunctionComponent = (props) => {
 
       <Box bg={baseColors.white} borderRadius={1} p={3}>
         <Box width="100%" mb={2}>
-          <Button onClick={createIdentifier}>Create Identifier</Button>
-        </Box>
-        <Box width="100%" mb={2}>
-          <Button.Outline onClick={getIdentifiers}>Refresh Identifiers</Button.Outline>
+          <Button onClick={createIdentifier} disabled={identifiersLoading && !hasIdentifier}>
+            {identifiersLoading && !hasIdentifier ? <Loader color="white" /> : "Create Identifier"}
+          </Button>
         </Box>
         <pre>identifiers:</pre>
         {identifiers &&
           identifiers.map((id: any, i: number) => {
             return <pre key={i}>{id}</pre>;
           })}
-        <pre>{error ? `error: ${error}` : undefined}</pre>
+        <pre>{error || getIdentifiersError ? `error: ${error || getIdentifiersError}` : undefined}</pre>
 
         <Box my={10}>
-          {identifiers[0] ? (
+          {identifiers?.[0] ? (
             <Button onClick={() => setIsIssueModalOpen(true)}>Issue Credential</Button>
           ) : (
             <Tooltip message="You must have at least one identifier in order to issue a VC" placement="top">
@@ -82,7 +77,7 @@ export const TenantPage: React.FunctionComponent = (props) => {
             onClick={() => setIsIssueModalOpen(false)}
           />
           <Box p={4} width="480px" maxWidth="95%" maxHeight="95vh" style={{ overflowY: "auto" }}>
-            <IssueVc defaultIssuer={identifiers[0]} onComplete={() => setIsIssueModalOpen(false)} />
+            <IssueVc defaultIssuer={identifiers?.[0]} onComplete={() => setIsIssueModalOpen(false)} />
           </Box>
         </Card>
       </Modal>
