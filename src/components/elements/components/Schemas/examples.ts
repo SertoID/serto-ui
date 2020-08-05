d767058f3f1d92a69ba0a8664323460fe733b2a8
Toby Fox <tobyfox@gmail.com>
2020-08-05 00:45:37 -0700

    chore: Reorder/rename examples

--- src/components/elements/components/Schemas/examples.ts
+++ src/components/elements/components/Schemas/examples.ts
@@ -1,239 +1,43 @@
 export const EXAMPLE_SCHEMAS: { [key: string]: string } = {
-  VerifiableCredential: `{
+  DiplomaCredential: `{
   "@context": {
     "@version": 1.1,
-    "@protected": true,
-
-    "id": "@id",
-    "type": "@type",
-
-    "VerifiableCredential": {
-      "@id": "https://www.w3.org/2018/credentials#VerifiableCredential",
+    "@rootType": "DiplomaCredential",
+    "w3ccred": "https://www.w3.org/2018/credentials#",
+    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+    "schema-id": "https://consensysidentity.com/schema/DiplomaCredential#",
+    "DiplomaCredential": {
+      "@id": "schema-id",
+      "@contains": "credentialSubject"
+    },
+    "credentialSubject": {
+      "@id": "w3ccred:credentialSubject",
+      "@required": true,
+      "@contains": ["alumniOf", "universityId", "title"],
       "@context": {
-        "@version": 1.1,
-        "@protected": true,
-
-        "id": "@id",
-        "type": "@type",
-
-        "cred": "https://www.w3.org/2018/credentials#",
-        "sec": "https://w3id.org/security#",
-        "xsd": "http://www.w3.org/2001/XMLSchema#",
-
-        "credentialSchema": {
-          "@id": "cred:credentialSchema",
+        "id": {
+          "@id": "@id",
           "@type": "@id",
-          "@context": {
-            "@version": 1.1,
-            "@protected": true,
-
-            "id": "@id",
-            "type": "@type",
-
-            "cred": "https://www.w3.org/2018/credentials#",
-
-            "JsonSchemaValidator2018": "cred:JsonSchemaValidator2018"
-          }
-        },
-        "credentialStatus": {"@id": "cred:credentialStatus", "@type": "@id"},
-        "credentialSubject": {"@id": "cred:credentialSubject", "@type": "@id"},
-        "evidence": {"@id": "cred:evidence", "@type": "@id"},
-        "expirationDate": {"@id": "cred:expirationDate", "@type": "xsd:dateTime"},
-        "holder": {"@id": "cred:holder", "@type": "@id"},
-        "issued": {"@id": "cred:issued", "@type": "xsd:dateTime"},
-        "issuer": {"@id": "cred:issuer", "@type": "@id"},
-        "issuanceDate": {"@id": "cred:issuanceDate", "@type": "xsd:dateTime"},
-        "proof": {"@id": "sec:proof", "@type": "@id", "@container": "@graph"},
-        "refreshService": {
-          "@id": "cred:refreshService",
-          "@type": "@id",
-          "@context": {
-            "@version": 1.1,
-            "@protected": true,
-
-            "id": "@id",
-            "type": "@type",
-
-            "cred": "https://www.w3.org/2018/credentials#",
-
-            "ManualRefreshService2018": "cred:ManualRefreshService2018"
-          }
-        },
-        "termsOfUse": {"@id": "cred:termsOfUse", "@type": "@id"},
-        "validFrom": {"@id": "cred:validFrom", "@type": "xsd:dateTime"},
-        "validUntil": {"@id": "cred:validUntil", "@type": "xsd:dateTime"}
+          "@required": true
+        }
       }
     },
-
-    "VerifiablePresentation": {
-      "@id": "https://www.w3.org/2018/credentials#VerifiablePresentation",
-      "@context": {
-        "@version": 1.1,
-        "@protected": true,
-
-        "id": "@id",
-        "type": "@type",
-
-        "cred": "https://www.w3.org/2018/credentials#",
-        "sec": "https://w3id.org/security#",
-
-        "holder": {"@id": "cred:holder", "@type": "@id"},
-        "proof": {"@id": "sec:proof", "@type": "@id", "@container": "@graph"},
-        "verifiableCredential": {"@id": "cred:verifiableCredential", "@type": "@id", "@container": "@graph"}
-      }
+    "alumniOf": {
+      "@id": "schema-id:alumniOf",
+      "@type": "rdf:HTML",
+      "@required": true,
+      "@dataType": "string"
     },
-
-    "EcdsaSecp256k1Signature2019": {
-      "@id": "https://w3id.org/security#EcdsaSecp256k1Signature2019",
-      "@context": {
-        "@version": 1.1,
-        "@protected": true,
-
-        "id": "@id",
-        "type": "@type",
-
-        "sec": "https://w3id.org/security#",
-        "xsd": "http://www.w3.org/2001/XMLSchema#",
-
-        "challenge": "sec:challenge",
-        "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
-        "domain": "sec:domain",
-        "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
-        "jws": "sec:jws",
-        "nonce": "sec:nonce",
-        "proofPurpose": {
-          "@id": "sec:proofPurpose",
-          "@type": "@vocab",
-          "@context": {
-            "@version": 1.1,
-            "@protected": true,
-
-            "id": "@id",
-            "type": "@type",
-
-            "sec": "https://w3id.org/security#",
-
-            "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
-            "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
-          }
-        },
-        "proofValue": "sec:proofValue",
-        "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"}
-      }
+    "universityId": {
+      "@id": "schema-id:universityId",
+      "@type": "rdf:langString",
+      "@dataType": "string"
     },
-
-    "EcdsaSecp256r1Signature2019": {
-      "@id": "https://w3id.org/security#EcdsaSecp256r1Signature2019",
-      "@context": {
-        "@version": 1.1,
-        "@protected": true,
-
-        "id": "@id",
-        "type": "@type",
-
-        "sec": "https://w3id.org/security#",
-        "xsd": "http://www.w3.org/2001/XMLSchema#",
-
-        "challenge": "sec:challenge",
-        "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
-        "domain": "sec:domain",
-        "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
-        "jws": "sec:jws",
-        "nonce": "sec:nonce",
-        "proofPurpose": {
-          "@id": "sec:proofPurpose",
-          "@type": "@vocab",
-          "@context": {
-            "@version": 1.1,
-            "@protected": true,
-
-            "id": "@id",
-            "type": "@type",
-
-            "sec": "https://w3id.org/security#",
-
-            "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
-            "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
-          }
-        },
-        "proofValue": "sec:proofValue",
-        "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"}
-      }
-    },
-
-    "Ed25519Signature2018": {
-      "@id": "https://w3id.org/security#Ed25519Signature2018",
-      "@context": {
-        "@version": 1.1,
-        "@protected": true,
-
-        "id": "@id",
-        "type": "@type",
-
-        "sec": "https://w3id.org/security#",
-        "xsd": "http://www.w3.org/2001/XMLSchema#",
-
-        "challenge": "sec:challenge",
-        "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
-        "domain": "sec:domain",
-        "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
-        "jws": "sec:jws",
-        "nonce": "sec:nonce",
-        "proofPurpose": {
-          "@id": "sec:proofPurpose",
-          "@type": "@vocab",
-          "@context": {
-            "@version": 1.1,
-            "@protected": true,
-
-            "id": "@id",
-            "type": "@type",
-
-            "sec": "https://w3id.org/security#",
-
-            "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
-            "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
-          }
-        },
-        "proofValue": "sec:proofValue",
-        "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"}
-      }
-    },
-
-    "RsaSignature2018": {
-      "@id": "https://w3id.org/security#RsaSignature2018",
-      "@context": {
-        "@version": 1.1,
-        "@protected": true,
-
-        "challenge": "sec:challenge",
-        "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
-        "domain": "sec:domain",
-        "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
-        "jws": "sec:jws",
-        "nonce": "sec:nonce",
-        "proofPurpose": {
-          "@id": "sec:proofPurpose",
-          "@type": "@vocab",
-          "@context": {
-            "@version": 1.1,
-            "@protected": true,
-
-            "id": "@id",
-            "type": "@type",
-
-            "sec": "https://w3id.org/security#",
-
-            "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
-            "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
-          }
-        },
-        "proofValue": "sec:proofValue",
-        "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"}
-      }
-    },
-
-    "proof": {"@id": "https://w3id.org/security#proof", "@type": "@id", "@container": "@graph"}
+    "title": {
+      "@id": "schema-id:title",
+      "@type": "rdf:HTML",
+      "@dataType": "string"
+    }
   }
 }`,
   ContentPublishCredential: `{
@@ -533,50 +337,261 @@ export const EXAMPLE_SCHEMAS: { [key: string]: string } = {
     }
   }
 }`,
-  DiplomaCredential: `{
+  "W3C base credentials context": `{
   "@context": {
     "@version": 1.1,
-    "@rootType": "DiplomaCredential",
-    "w3ccred": "https://www.w3.org/2018/credentials#",
-    "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-    "schema-id": "https://consensysidentity.com/schema/DiplomaCredential#",
-    "DiplomaCredential": {
-      "@id": "schema-id",
-      "@contains": "credentialSubject"
-    },
-    "credentialSubject": {
-      "@id": "w3ccred:credentialSubject",
-      "@required": true,
-      "@contains": ["alumniOf", "universityId", "title"],
+    "@protected": true,
+
+    "id": "@id",
+    "type": "@type",
+
+    "VerifiableCredential": {
+      "@id": "https://www.w3.org/2018/credentials#VerifiableCredential",
       "@context": {
-        "id": {
-          "@id": "@id",
+        "@version": 1.1,
+        "@protected": true,
+
+        "id": "@id",
+        "type": "@type",
+
+        "cred": "https://www.w3.org/2018/credentials#",
+        "sec": "https://w3id.org/security#",
+        "xsd": "http://www.w3.org/2001/XMLSchema#",
+
+        "credentialSchema": {
+          "@id": "cred:credentialSchema",
           "@type": "@id",
-          "@required": true
-        }
+          "@context": {
+            "@version": 1.1,
+            "@protected": true,
+
+            "id": "@id",
+            "type": "@type",
+
+            "cred": "https://www.w3.org/2018/credentials#",
+
+            "JsonSchemaValidator2018": "cred:JsonSchemaValidator2018"
+          }
+        },
+        "credentialStatus": {"@id": "cred:credentialStatus", "@type": "@id"},
+        "credentialSubject": {"@id": "cred:credentialSubject", "@type": "@id"},
+        "evidence": {"@id": "cred:evidence", "@type": "@id"},
+        "expirationDate": {"@id": "cred:expirationDate", "@type": "xsd:dateTime"},
+        "holder": {"@id": "cred:holder", "@type": "@id"},
+        "issued": {"@id": "cred:issued", "@type": "xsd:dateTime"},
+        "issuer": {"@id": "cred:issuer", "@type": "@id"},
+        "issuanceDate": {"@id": "cred:issuanceDate", "@type": "xsd:dateTime"},
+        "proof": {"@id": "sec:proof", "@type": "@id", "@container": "@graph"},
+        "refreshService": {
+          "@id": "cred:refreshService",
+          "@type": "@id",
+          "@context": {
+            "@version": 1.1,
+            "@protected": true,
+
+            "id": "@id",
+            "type": "@type",
+
+            "cred": "https://www.w3.org/2018/credentials#",
+
+            "ManualRefreshService2018": "cred:ManualRefreshService2018"
+          }
+        },
+        "termsOfUse": {"@id": "cred:termsOfUse", "@type": "@id"},
+        "validFrom": {"@id": "cred:validFrom", "@type": "xsd:dateTime"},
+        "validUntil": {"@id": "cred:validUntil", "@type": "xsd:dateTime"}
       }
     },
-    "alumniOf": {
-      "@id": "schema-id:alumniOf",
-      "@type": "rdf:HTML",
-      "@required": true,
-      "@dataType": "string"
+
+    "VerifiablePresentation": {
+      "@id": "https://www.w3.org/2018/credentials#VerifiablePresentation",
+      "@context": {
+        "@version": 1.1,
+        "@protected": true,
+
+        "id": "@id",
+        "type": "@type",
+
+        "cred": "https://www.w3.org/2018/credentials#",
+        "sec": "https://w3id.org/security#",
+
+        "holder": {"@id": "cred:holder", "@type": "@id"},
+        "proof": {"@id": "sec:proof", "@type": "@id", "@container": "@graph"},
+        "verifiableCredential": {"@id": "cred:verifiableCredential", "@type": "@id", "@container": "@graph"}
+      }
     },
-    "universityId": {
-      "@id": "schema-id:universityId",
-      "@type": "rdf:langString",
-      "@dataType": "string"
+
+    "EcdsaSecp256k1Signature2019": {
+      "@id": "https://w3id.org/security#EcdsaSecp256k1Signature2019",
+      "@context": {
+        "@version": 1.1,
+        "@protected": true,
+
+        "id": "@id",
+        "type": "@type",
+
+        "sec": "https://w3id.org/security#",
+        "xsd": "http://www.w3.org/2001/XMLSchema#",
+
+        "challenge": "sec:challenge",
+        "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
+        "domain": "sec:domain",
+        "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
+        "jws": "sec:jws",
+        "nonce": "sec:nonce",
+        "proofPurpose": {
+          "@id": "sec:proofPurpose",
+          "@type": "@vocab",
+          "@context": {
+            "@version": 1.1,
+            "@protected": true,
+
+            "id": "@id",
+            "type": "@type",
+
+            "sec": "https://w3id.org/security#",
+
+            "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
+            "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
+          }
+        },
+        "proofValue": "sec:proofValue",
+        "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"}
+      }
     },
-    "title": {
-      "@id": "schema-id:title",
-      "@type": "rdf:HTML",
-      "@dataType": "string"
-    }
+
+    "EcdsaSecp256r1Signature2019": {
+      "@id": "https://w3id.org/security#EcdsaSecp256r1Signature2019",
+      "@context": {
+        "@version": 1.1,
+        "@protected": true,
+
+        "id": "@id",
+        "type": "@type",
+
+        "sec": "https://w3id.org/security#",
+        "xsd": "http://www.w3.org/2001/XMLSchema#",
+
+        "challenge": "sec:challenge",
+        "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
+        "domain": "sec:domain",
+        "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
+        "jws": "sec:jws",
+        "nonce": "sec:nonce",
+        "proofPurpose": {
+          "@id": "sec:proofPurpose",
+          "@type": "@vocab",
+          "@context": {
+            "@version": 1.1,
+            "@protected": true,
+
+            "id": "@id",
+            "type": "@type",
+
+            "sec": "https://w3id.org/security#",
+
+            "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
+            "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
+          }
+        },
+        "proofValue": "sec:proofValue",
+        "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"}
+      }
+    },
+
+    "Ed25519Signature2018": {
+      "@id": "https://w3id.org/security#Ed25519Signature2018",
+      "@context": {
+        "@version": 1.1,
+        "@protected": true,
+
+        "id": "@id",
+        "type": "@type",
+
+        "sec": "https://w3id.org/security#",
+        "xsd": "http://www.w3.org/2001/XMLSchema#",
+
+        "challenge": "sec:challenge",
+        "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
+        "domain": "sec:domain",
+        "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
+        "jws": "sec:jws",
+        "nonce": "sec:nonce",
+        "proofPurpose": {
+          "@id": "sec:proofPurpose",
+          "@type": "@vocab",
+          "@context": {
+            "@version": 1.1,
+            "@protected": true,
+
+            "id": "@id",
+            "type": "@type",
+
+            "sec": "https://w3id.org/security#",
+
+            "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
+            "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
+          }
+        },
+        "proofValue": "sec:proofValue",
+        "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"}
+      }
+    },
+
+    "RsaSignature2018": {
+      "@id": "https://w3id.org/security#RsaSignature2018",
+      "@context": {
+        "@version": 1.1,
+        "@protected": true,
+
+        "challenge": "sec:challenge",
+        "created": {"@id": "http://purl.org/dc/terms/created", "@type": "xsd:dateTime"},
+        "domain": "sec:domain",
+        "expires": {"@id": "sec:expiration", "@type": "xsd:dateTime"},
+        "jws": "sec:jws",
+        "nonce": "sec:nonce",
+        "proofPurpose": {
+          "@id": "sec:proofPurpose",
+          "@type": "@vocab",
+          "@context": {
+            "@version": 1.1,
+            "@protected": true,
+
+            "id": "@id",
+            "type": "@type",
+
+            "sec": "https://w3id.org/security#",
+
+            "assertionMethod": {"@id": "sec:assertionMethod", "@type": "@id", "@container": "@set"},
+            "authentication": {"@id": "sec:authenticationMethod", "@type": "@id", "@container": "@set"}
+          }
+        },
+        "proofValue": "sec:proofValue",
+        "verificationMethod": {"@id": "sec:verificationMethod", "@type": "@id"}
+      }
+    },
+
+    "proof": {"@id": "https://w3id.org/security#proof", "@type": "@id", "@container": "@graph"}
   }
 }`,
 };
 
 export const EXAMPLE_VCS: { [key: string]: string } = {
+  DiplomaCredential: `{
+  "@context": [
+    "https://www.w3.org/2018/credentials/v1",
+    "https://www.w3.org/2018/credentials/examples/v1"
+  ],
+  "type": ["VerifiableCredential", "DiplomaCredential"],
+  "issuer": "did:ethr:rinkeby:0x9fb04797cc0b1711c86b960105e0c3ed3f9cb749",
+  "issuanceDate": "2017-12-05T14:27:42Z",
+  "credentialSubject": {
+    "id": "did:ethr:rinkeby:0x9fb04797cc0b1711c86b960105e0c3ed3f9cb749",
+    "title": "Diploma Credential",
+    "alumniOf": "Example University",
+    "universityId": "did:example:c276e12ec21"
+  }
+}`,
   ContentPublishCredential: `{
   "@context": [
     "https://www.w3.org/2018/credentials/v1"
@@ -661,21 +676,6 @@ export const EXAMPLE_VCS: { [key: string]: string } = {
     "publishedContent": {
       "url": "https://example-publisher.com/articles/a-very-important-article"
     }
-  }
-}`,
-  DiplomaCredential: `{
-  "@context": [
-    "https://www.w3.org/2018/credentials/v1",
-    "https://www.w3.org/2018/credentials/examples/v1"
-  ],
-  "type": ["VerifiableCredential", "DiplomaCredential"],
-  "issuer": "did:ethr:rinkeby:0x9fb04797cc0b1711c86b960105e0c3ed3f9cb749",
-  "issuanceDate": "2017-12-05T14:27:42Z",
-  "credentialSubject": {
-    "id": "did:ethr:rinkeby:0x9fb04797cc0b1711c86b960105e0c3ed3f9cb749",
-    "title": "Diploma Credential",
-    "alumniOf": "Example University",
-    "universityId": "did:example:c276e12ec21"
   }
 }`,
 };
--- src/components/elements/components/Schemas/utils.stories.tsx
+++ src/components/elements/components/Schemas/utils.stories.tsx
@@ -50,7 +50,7 @@ const CodeWrap = styled(Card)`
   }
 `;
 
-storiesOf("Schemas", module).add("Definition Demo", () => {
+storiesOf("Schemas", module).add("@context+ Playground", () => {
   const [inputSchema, setInputSchema] = React.useState<string>("");
   const [debouncedSchema] = useDebounce(inputSchema, 500);
   const [inputSchemaError, setInputSchemaError] = React.useState<any>();
