1d6e1ed556598a6f104688533fd44c287c1e854b
Toby Fox <tobyfox@gmail.com>
2021-01-27 23:00:48 -0800

    build: Make it an actual library

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+// NOTE: This is the entry point for create-react-app webserver. The entry point for actual Serto UI components for export is `serto-ui.ts`.
+
 import React from "react";
 import ReactDOM from "react-dom";
 import { App } from "./App";
