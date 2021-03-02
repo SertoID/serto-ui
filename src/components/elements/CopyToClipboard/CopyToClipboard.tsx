8673654e411e8317e4d376dc206c0869b34968d8
sruddy <sarah.ruddy@gmail.com>
2021-03-02 18:36:31 -0500

    chore: Update CopyToClipboard

--- src/components/elements/CopyToClipboard/CopyToClipboard.tsx
+++ src/components/elements/CopyToClipboard/CopyToClipboard.tsx
@@ -1,13 +1,25 @@
 import * as React from "react";
+import styled from "styled-components";
 import { Button } from "rimble-ui";
 import { colors } from "../../../themes";
 import { copyToClipboard } from "../../../utils";
 import { ContentCopy, Check } from "../Icons";
 
+export const ContentCopyStyled = styled.span`
+  cursor: pointer;
+  &:hover svg {
+    transition: 250ms ease;
+    fill: #5952ff;
+  }
+`;
+
 export interface CopyToClipboardProps {
   text: string;
+  color?: string;
+  hoverTitle?: string;
   size?: string;
   textButton?: boolean;
+  textButtonTitle?: string;
 }
 
 export const CopyToClipboard: React.FunctionComponent<CopyToClipboardProps> = (props) => {
@@ -23,23 +35,26 @@ export const CopyToClipboard: React.FunctionComponent<CopyToClipboardProps> = (p
 
   if (copied) {
     if (props.textButton) {
-      return <Button.Outline size="small">{"Copied"}</Button.Outline>;
+      return <Button.Outline size="small">Copied</Button.Outline>;
     }
     return <Check color={colors.success.base} size={props.size} />;
   }
 
   if (props.textButton) {
     return (
-      <Button size="small" onClick={() => setCopied(copyToClipboard(props.text))}>
-        {"Copy"}
-      </Button>
+      <Button.Outline color={props.color} onClick={() => setCopied(copyToClipboard(props.text))} size="small">
+        {props.textButtonTitle || "Copy"}
+      </Button.Outline>
     );
   }
   return (
-    <ContentCopy
-      onClick={() => setCopied(copyToClipboard(props.text))}
-      size={props.size}
-      style={{ cursor: "pointer" }}
-    />
+    <ContentCopyStyled title={props.hoverTitle}>
+      <ContentCopy
+        color={props.color}
+        onClick={() => setCopied(copyToClipboard(props.text))}
+        size={props.size}
+        style={{ cursor: "pointer" }}
+      />
+    </ContentCopyStyled>
   );
 };
