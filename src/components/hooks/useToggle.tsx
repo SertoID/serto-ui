2d48854d0e46346a4e05eb0af06351340ab438a0
sruddy <sarah.ruddy@gmail.com>
2021-03-04 17:30:42 -0500

    feat: hooks useToggle and useWindowSize

---
+++ src/components/hooks/useToggle.tsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+
+export const useToggle = (initialValue: boolean): [boolean, () => void] => {
+  const [value, setValue] = useState(initialValue);
+  const toggleValue = () => setValue(!value);
+  return [value, toggleValue];
+};
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./elements";
+export * from "./hooks";
 export * from "./layouts";
 export * from "./views";
