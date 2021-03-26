8962e50ce51043ee06e9d8dae50d6cc0bf57b4fc
Ese Mentie <ese.mentie@gmail.com>
2021-03-26 02:10:32 +0100

    test: Add sample test and codecov config

---
+++ jest.config.js
@@ -0,0 +1,34 @@
+// For a detailed explanation regarding each configuration property, visit:
+// https://jestjs.io/docs/en/configuration.html
+
+// eslint-disable-next-line node/exports-style
+module.exports = {
+  clearMocks: true,
+  collectCoverage: true,
+  coveragePathIgnorePatterns: [
+    'index.ts',
+    '/node_modules/'
+  ],
+
+  // An array of file extensions your modules use
+  moduleFileExtensions: [
+    'js',
+    'json',
+    'jsx',
+    'ts',
+    'tsx',
+    'node'
+  ],
+
+ 
+  testEnvironment: 'node',
+
+  // The glob patterns Jest uses to detect test files
+  testMatch: [
+    '**/src/**/?(*.)+(spec|test).[tj]s?(x)'
+  ],
+  // A map from regular expressions to paths to transformers
+  transform: {
+    '\\.(ts)$': 'ts-jest'
+  }
+}
