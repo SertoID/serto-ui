e02ca49a5fadd21e6881a26d56dc8ed5e3f1d2c6
Eseoghene Mentie <ese.mentie@gmail.com>
2021-03-29 15:14:58 +0100

    chore:Fix prettier errors

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,22 +1,19 @@
 import * as React from "react";
 import { App } from "./App";
-import { render, waitFor, screen } from '@testing-library/react'
+import { render, waitFor, screen } from "@testing-library/react";
 
 it("renders without crashing", async () => {
-  
- render(<App />);
- await waitFor(() => screen.getByRole('heading'));
- const Home = screen.getByText('Home');
- const Nowhere = screen.getByText('Nowhere');
- const Everywhere = screen.getByText('Everywhere'); 
+  render(<App />);
+  await waitFor(() => screen.getByRole("heading"));
+  const Home = screen.getByText("Home");
+  const Nowhere = screen.getByText("Nowhere");
+  const Everywhere = screen.getByText("Everywhere");
 
- expect(screen.getByRole('heading')).toHaveTextContent('Serto UI');
- expect(Home).toHaveTextContent('Home');
- expect(Nowhere).toHaveTextContent('Nowhere');
- expect(Everywhere).toHaveTextContent('Everywhere');
- expect(Nowhere.closest('a')).toHaveAttribute('href', '/nowhere');
- expect(Everywhere.closest('a')).toHaveAttribute('href', '/everywhere');
- expect(screen.getByText('Welcome!')).toHaveTextContent('Welcome!')
-
-
+  expect(screen.getByRole("heading")).toHaveTextContent("Serto UI");
+  expect(Home).toHaveTextContent("Home");
+  expect(Nowhere).toHaveTextContent("Nowhere");
+  expect(Everywhere).toHaveTextContent("Everywhere");
+  expect(Nowhere.closest("a")).toHaveAttribute("href", "/nowhere");
+  expect(Everywhere.closest("a")).toHaveAttribute("href", "/everywhere");
+  expect(screen.getByText("Welcome!")).toHaveTextContent("Welcome!");
 });
