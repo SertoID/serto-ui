ab986ee4ab0dfcd10e9715d8cebe26a9b9f96acd
Toby Fox <tobyfox@gmail.com>
2021-05-13 21:53:29 -0700

    style: Credential component padding/margin

--- src/components/elements/Icons/SertoVerifiedCheckmark.tsx
+++ src/components/elements/Icons/SertoVerifiedCheckmark.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { SvgProps } from "./";
 
-export const SertoVerifiedCheckmark: React.FunctionComponent<SvgProps> = () => {
+export const SertoVerifiedCheckmark: React.FunctionComponent<SvgProps> = (props) => {
   return (
     <svg width="55" height="55" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
