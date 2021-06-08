dde1d35543b6483c3ad503600d0a15ac4d890a7b
sruddy <sarah.ruddy@gmail.com>
2021-06-08 10:41:14 -0400

    chore: Update url

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ const config = {
   ENVIRONMENT: process.env.NODE_ENV || "development",
   DEFAULT_SCHEMA_API_URL: "https://staging.api.schemas.serto.id",
   DEFAULT_CREATE_SCHEMA_PATH: "/schemas/",
-  DEFAULT_SEARCH_API_URL: "https://staging.api.search.serto.id",
+  DEFAULT_SEARCH_API_URL: "https://beta.api.search.serto.id",
 };
 
 export { config };
