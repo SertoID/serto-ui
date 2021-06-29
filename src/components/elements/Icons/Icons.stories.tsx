e3ca6cde4f6c19d810fcf7a447633b27db0c9583
sruddy <sarah.ruddy@gmail.com>
2021-06-29 13:42:41 -0400

    chore: Remove Rimble icons

--- src/components/elements/Icons/Icons.stories.tsx
+++ src/components/elements/Icons/Icons.stories.tsx
@@ -1,54 +1,23 @@
 import { storiesOf } from "@storybook/react";
 import {
-  AddCircle,
-  ArrowBack,
-  Check,
-  Code,
-  ContentCopy,
   CredentialCheck,
   CredentialCheckFilled,
   DidKeyIcon,
   DidSovIcon,
   DidWebIcon,
-  EthrDidLogo,
   GitHub,
   GreenCircleCheck,
-  Home,
-  Info,
-  KeyboardArrowDown,
-  KeyboardArrowUp,
   OutlineOne,
   OutlineTwo,
-  People,
-  Person,
-  SelectAll,
-  Send,
   SertoAgentLogo,
   SertoLogo,
   SertoSchemasLogo,
   SertoSearchLogo,
   SertoVerifiedCheckmark,
   SertoVerificationError,
-  Settings,
-  VerifiedUser,
 } from "./";
 
 storiesOf("Icons", module)
-  .add("AddCircle", () => {
-    return <AddCircle />;
-  })
-  .add("ArrowBack", () => {
-    return <ArrowBack />;
-  })
-  .add("Check", () => {
-    return <Check />;
-  })
-  .add("Code", () => {
-    return <Code />;
-  })
-  .add("ContentCopy", () => {
-    return <ContentCopy />;
-  })
   .add("CredentialCheck", () => {
     return <CredentialCheck />;
   })
@@ -64,38 +33,20 @@ storiesOf("Icons", module)
   .add("DidWebIcon", () => {
     return <DidWebIcon />;
   })
-  .add("EthrDidLogo", () => {
-    return <EthrDidLogo />;
-  })
   .add("GitHub", () => {
     return <GitHub />;
   })
-  .add("Home", () => {
-    return <Home />;
-  })
-  .add("Info", () => {
-    return <Info />;
-  })
-  .add("KeyboardArrowDown", () => {
-    return <KeyboardArrowDown />;
-  })
-  .add("KeyboardArrowUp", () => {
-    return <KeyboardArrowUp />;
-  })
-  .add("People", () => {
-    return <People />;
-  })
-  .add("Person", () => {
-    return <Person />;
-  })
-  .add("SelectAll", () => {
-    return <SelectAll />;
-  })
-  .add("Send", () => {
-    return <Send />;
-  })
   .add("SertoAgentLogo", () => {
     return <SertoAgentLogo />;
+  })
+  .add("GreenCircleCheck", () => {
+    return <GreenCircleCheck />;
+  })
+  .add("OutlineOne", () => {
+    return <OutlineOne />;
+  })
+  .add("OutlineTwo", () => {
+    return <OutlineTwo />;
   })
   .add("SertoLogo", () => {
     return <SertoLogo />;
@@ -106,24 +57,9 @@ storiesOf("Icons", module)
   .add("SertoSearchLogo", () => {
     return <SertoSearchLogo />;
   })
-  .add("Settings", () => {
-    return <Settings />;
-  })
-  .add("VerifiedUser", () => {
-    return <VerifiedUser />;
-  })
   .add("SertoVerificationError", () => {
     return <SertoVerificationError size="50px" />;
   })
   .add("SertoVerifiedCheckmark", () => {
     return <SertoVerifiedCheckmark size="50px" />;
-  })
-  .add("GreenCircleCheck", () => {
-    return <GreenCircleCheck />;
-  })
-  .add("OutlineOne", () => {
-    return <OutlineOne />;
-  })
-  .add("OutlineTwo", () => {
-    return <OutlineTwo />;
   });
