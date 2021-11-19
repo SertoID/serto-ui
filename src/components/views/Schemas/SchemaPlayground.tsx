318a929bd46e957ba966340c1f58846fa027c50c
Toby Fox <tobyfox@gmail.com>
2021-11-19 17:30:05 +0000

    feat: Update Schema Playground to work with new format

--- src/components/views/Schemas/SchemaPlayground.tsx
+++ src/components/views/Schemas/SchemaPlayground.tsx
@@ -10,10 +10,14 @@ import { useDebounce } from "use-debounce";
 import { H5 } from "../../layouts";
 import { HighlightedJson, PrismHighlightedCodeWrap } from "../../elements/HighlightedJson/HighlightedJson";
 
-const Section = styled(Flex)`
+const Pane = styled(Flex)`
   flex-direction: column;
   width: 50%;
-  height: 48vh;
+  height: 94vh;
+`;
+const Section = styled(Flex)`
+  flex-direction: column;
+  height: 50%;
 `;
 
 export const SchemaPlayground: React.FunctionComponent = () => {
@@ -28,7 +32,6 @@ export const SchemaPlayground: React.FunctionComponent = () => {
   const [inputVcValidityMessage, setInputVcValidityMessage] = React.useState<string | undefined>();
 
   const [outputContext, setOutputContext] = React.useState<string>("");
-  const [outputJsonSchema, setOutputJsonSchema] = React.useState<string>("");
 
   React.useEffect(() => {
     setInputSchemaError(undefined);
@@ -44,22 +47,15 @@ export const SchemaPlayground: React.FunctionComponent = () => {
   }, [debouncedSchema]);
 
   React.useEffect(() => {
-    if (vcSchema) {
-      setOutputContext(vcSchema.getJsonLdContextString(true));
-      setOutputJsonSchema(vcSchema.getJsonSchemaString(true));
-    } else {
-      setOutputContext("");
-      setOutputJsonSchema("");
-    }
+    setOutputContext(vcSchema ? vcSchema.getJsonLdContextString(true) : "");
   }, [vcSchema]);
 
   React.useEffect(() => {
     if (vcSchema && debouncedVc) {
       (async function () {
-        vcSchema.validateVc(debouncedVc, (isValid, message) => {
-          setInputVcValid(isValid);
-          setInputVcValidityMessage(message);
-        });
+        const { isValid, message } = await vcSchema.validateVc(debouncedVc);
+        setInputVcValid(isValid);
+        setInputVcValidityMessage(message);
       })();
     } else {
       setInputVcValid(undefined);
@@ -74,10 +70,10 @@ export const SchemaPlayground: React.FunctionComponent = () => {
           onChange={(event: any) => {
             const name = event.target.value;
             if (name) {
-              setInputSchema(EXAMPLE_SCHEMAS[name]);
+              setInputSchema(JSON.stringify(EXAMPLE_SCHEMAS[name], null, 2));
             }
             if (EXAMPLE_VCS[name]) {
-              setInputVc(EXAMPLE_VCS[name]);
+              setInputVc(JSON.stringify(EXAMPLE_VCS[name], null, 2));
             }
           }}
         >
@@ -90,10 +86,8 @@ export const SchemaPlayground: React.FunctionComponent = () => {
         </select>
       </Text>
       <Flex>
-        <Section>
-          <Flex justifyContent="space-between" pr={2}>
-            <H5 my={2}>@context+ input</H5>
-          </Flex>
+        <Pane>
+          <H5 my={2}>JSON Schema input</H5>
           <PrismHighlightedCodeWrap style={{ background: "#fdfdfd" }}>
             <Editor
               value={inputSchema}
@@ -103,52 +97,47 @@ export const SchemaPlayground: React.FunctionComponent = () => {
             />
           </PrismHighlightedCodeWrap>
           {inputSchemaError && <Flash variant="danger">Error: {inputSchemaError}</Flash>}
-        </Section>
-        <Section>
-          <Flex justifyContent="space-between" pr={2}>
-            <H5 my={2}>VC input for validation</H5>
-            <Text my={2} fontSize={1}>
-              playground @context will automatically be appended
-            </Text>
-          </Flex>
-          <PrismHighlightedCodeWrap style={{ background: "#fdfdfd" }}>
-            <Editor
-              value={inputVc}
-              onValueChange={setInputVc}
-              highlight={() => Prism.highlight(inputVc, Prism.languages.json, "json")}
-              style={{ minHeight: "100%" }}
-            />
-          </PrismHighlightedCodeWrap>
-          {typeof inputVcValid !== "undefined" && (
-            <Flash variant={inputVcValid ? "success" : inputVcValid === null ? "warning" : "danger"}>
-              <Text fontSize={1} style={{ wordBreak: "break-word", float: "left", display: "inline-block" }}>
-                {inputVcValidityMessage}
+        </Pane>
+        <Pane>
+          <Section>
+            <Flex justifyContent="space-between" pr={2}>
+              <H5 my={2}>VC input for validation</H5>
+              <Text my={2} fontSize={1}>
+                playground @context will automatically be appended
               </Text>
-              <Tooltip placement="top" message="@context output below left will be added to the VC sent to the tool">
-                <Text fontSize={1} style={{ float: "right", display: "inline-block" }}>
-                  Open in{" "}
-                  <Button.Outline size="small" onClick={() => vcSchema?.openGoogleJsonLdValidatorPage(inputVc)}>
-                    Google JSON-LD Tool
-                  </Button.Outline>
-                  <Button.Outline size="small" ml={2} onClick={() => vcSchema?.openJsonLdPlaygroundPage(inputVc)}>
-                    JSON-LD Playground
-                  </Button.Outline>
+            </Flex>
+            <PrismHighlightedCodeWrap style={{ background: "#fdfdfd" }}>
+              <Editor
+                value={inputVc}
+                onValueChange={setInputVc}
+                highlight={() => Prism.highlight(inputVc, Prism.languages.json, "json")}
+                style={{ minHeight: "100%" }}
+              />
+            </PrismHighlightedCodeWrap>
+            {typeof inputVcValid !== "undefined" && (
+              <Flash variant={inputVcValid ? "success" : inputVcValid === null ? "warning" : "danger"}>
+                <Text fontSize={1} style={{ wordBreak: "break-word", float: "left", display: "inline-block" }}>
+                  {inputVcValidityMessage}
                 </Text>
-              </Tooltip>
-            </Flash>
-          )}
-        </Section>
-      </Flex>
-      <Flex>
-        <Section>
-          <H5 my={2}>JSON-LD @context output</H5>
-          <HighlightedJson json={outputContext} alreadyPrettified={true} />
-        </Section>
-        <Section>
-          <H5 my={2}>JSON Schema output</H5>
-          <HighlightedJson json={outputJsonSchema} alreadyPrettified={true} />
-          {vcSchema?.jsonSchemaMessage && <Flash variant="warning">{vcSchema.jsonSchemaMessage}</Flash>}
-        </Section>
+                <Tooltip placement="top" message="@context output below left will be added to the VC sent to the tool">
+                  <Text fontSize={1} style={{ float: "right", display: "inline-block" }}>
+                    Open in{" "}
+                    <Button.Outline size="small" onClick={() => vcSchema?.openGoogleJsonLdValidatorPage(inputVc)}>
+                      Google JSON-LD Tool
+                    </Button.Outline>
+                    <Button.Outline size="small" ml={2} onClick={() => vcSchema?.openJsonLdPlaygroundPage(inputVc)}>
+                      JSON-LD Playground
+                    </Button.Outline>
+                  </Text>
+                </Tooltip>
+              </Flash>
+            )}
+          </Section>
+          <Section>
+            <H5 my={2}>JSON-LD @context output</H5>
+            <HighlightedJson json={outputContext} alreadyPrettified={true} />
+          </Section>
+        </Pane>
       </Flex>
     </Box>
   );
